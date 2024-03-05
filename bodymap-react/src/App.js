2e0b839d29712952032d55824cdf73692b2075f2
vishal18713 <lcs2023052@iiitl.ac.in>
2024-03-05 12:35:17 +0530

    Add password input fields and body map functionality

--- bodymap-react/src/App.js
+++ bodymap-react/src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useState} from 'react';
+const Web3 = require('web3');
+window.ethereum.request({ method: 'eth_requestAccounts' });
+const web3 = new Web3(window.ethereum);
 
 function App() {
+  const[passwordClearText,setPasswordClearText] = useState('');
+  const[passwordClearTextBasic,setPasswordClearTextBasic] = useState('');
+  const[passwordClearTextTailor,setPasswordClearTextTailor] = useState('');
+
+  const handlePasswordClearTextChanged = (event) =>{
+    setPasswordClearText(event.target.value);
+
+  }
+  const passwordClearTextBasicChanged = (event) =>{
+    setPasswordClearTextBasic(event.target.value);
+  }
+
+  const passwordClearTextTailorChanged = (event) =>{
+    setPasswordClearTextTailor(event.target.value);
+  }
+  const [bodyMapBasic,setBodyMapBasic] = useState({
+    Height:'',Weight:''
+  });
+  const [bodyMapTailor,setBodyMapTailor] = useState({
+    Waist:'',Legs:'',Arms:'',Posture:''
+  });
+  const handleBodyMapBasicChange = (e,key) =>{
+    setBodyMapBasic({...bodyMapBasic,[key]:e.target.value});
+  }
+  const handleBodyMapTailorChange = (e,key) =>{
+    setBodyMapTailor({...bodyMapTailor,[key]:e.target.value});
+  }
+  
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
