1af801d031735db7fcd30019200804c1f7ff32b0
vishal18713 <lcs2023052@iiitl.ac.in>
2024-03-05 11:44:43 +0530

    Fix bug in login functionality

---
+++ bodymap-server/compile.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const fs = require('fs');
+const contractToCompileName = 'bodymap.sol';
+const contractPath = path.resolve(__dirname, 'contracts', contractToCompileName);
+const contractSource  = fs.readFileSync(contractPath,'utf-8');
+
+var input = 
+{
+    language: 'Solidity',
+    sources: {[contractToCompileName]: {content:contractSource}},
+    settings: {outputSelection: {'*': {'*': ['*']}}
+    }
+};
+
+const compiledContracts = JSON.parse(solc.compile(JSON.stringify(input)));
+console.log(compiledContracts);
+const compiledContract = compiledContracts.contracts[contractToCompileName]["BodyMap"];
+console.log(compiledContract);
+module.exports = {compiledContract};
