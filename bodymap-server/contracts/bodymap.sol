8b1d4d63971a0ac4f5e925efbe85d1a926361303
vishal18713 <lcs2023052@iiitl.ac.in>
2024-03-05 11:38:22 +0530

    Add setBodyMaps function to BodyMap contract

--- bodymap-server/contracts/bodymap.sol
+++ bodymap-server/contracts/bodymap.sol
@@ -8,4 +8,11 @@ contract BodyMap{
     constructor (string memory passwordClearText){
         passwordHash = keccak256(abi.encodePacked(passwordClearText));
     }
+    function setBodyMaps(string memory passwordClearText,string memory newBasicBodyMap,string memory newTailorBodyMap) public {
+        bytes32 givenPasswordHash = keccak256(abi.encodePacked(passwordClearText));
+        if(passwordHash == givenPasswordHash){
+            basicBodyMap = newBasicBodyMap;
+            tailorBodyMap = newTailorBodyMap;
+        }
+    }
 }
